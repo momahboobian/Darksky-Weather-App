2cc2a0c63870ae16a84b1373240ae5abb255b38b
Mo Mahboobian <momahboobian@gmail.com>
2023-02-17 10:46:21 +0000

    next 4 days forecast

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ window.addEventListener("load", () => {
           console.log(currentData);
           // update the DOM with the temperature value
           const { temp } = currentData.main;
+          tempAndConversion(temp);
           const country = currentData.sys.country;
           const city = currentData.name;
           const iconCode = currentData.weather[0].icon;
@@ -20,22 +21,18 @@ window.addEventListener("load", () => {
           const tempElement = document.querySelector(".temp");
           const { main, description } = currentData.weather[0];
 
-          tempElement.textContent = `${temp} K`;
-
-          const countryElement = document.querySelector(".country");
-          countryElement.textContent = `Country: ${country}`;
-
-          const cityElement = document.querySelector(".city");
-          cityElement.textContent = `City: ${city}`;
+          const countryElement = document.querySelector(".city-country");
+          countryElement.textContent = `${city}, ${country}`;
 
           const iconElement = document.querySelector(".weather-icon img");
-          iconElement.src = iconUrl;
+          // iconElement.src = iconUrl;
+          iconElement.src = "/src/weather-icons/animated/cloudy.svg";
 
-          const conditionElement = document.querySelector(".condition");
+          // const conditionElement = document.querySelector(".condition");
+          // conditionElement.textContent = `Condition: ${main}`;
+
           const descriptionElement = document.querySelector(".description");
-
-          conditionElement.textContent = `Condition: ${main}`;
-          descriptionElement.textContent = `Description: ${description}`;
+          descriptionElement.textContent = `${description}`;
 
           // Fetch the 5-day forecast data
           const forecastApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
@@ -53,6 +50,30 @@ window.addEventListener("load", () => {
     console.log("Geolocation is not supported by this browser.");
   }
 });
+// temp conversion
+function tempAndConversion(temp) {
+  const tempElement = document.querySelector(".temp");
+
+  let tempUnit = "celsius";
+  let tempValue = Math.round(temp - 273.15);
+  tempElement.textContent = `${tempValue}° C`;
+
+  tempElement.addEventListener("click", () => {
+    if (tempUnit === "celsius") {
+      tempUnit = "fahrenheit";
+      tempValue = Math.round((tempValue * 9) / 5 + 32);
+      tempElement.textContent = `${tempValue}° F`;
+    } else if (tempUnit === "fahrenheit") {
+      tempUnit = "kelvin";
+      tempValue = Math.round((tempValue + 459.67) * (5 / 9));
+      tempElement.textContent = `${tempValue}° K`;
+    } else {
+      tempUnit = "celsius";
+      tempValue = Math.round(temp - 273.15);
+      tempElement.textContent = `${tempValue}° C`;
+    }
+  });
+}
 
 function forecastFunc(data) {
   const forecastList = data.list;
@@ -68,32 +89,38 @@ function forecastFunc(data) {
     const dayName = date.toLocaleDateString("en-US", {
       weekday: "long",
     });
-    const icon = day.weather[0].icon;
+    // const icon = day.weather[0].icon;
     const temp = Math.round(day.main.temp);
-
-    const dayElement = document.createElement("div");
-    dayElement.classList.add("day");
-
-    const dayNameElement = document.createElement("div");
-    dayNameElement.classList.add("day-name");
-    dayNameElement.textContent = dayName;
-
-    const dateElement = document.createElement("div");
-    dateElement.classList.add("date");
-    dateElement.textContent = date.toLocaleDateString("en-US");
 
     const iconElement = document.createElement("img");
     iconElement.classList.add("icon");
-    iconElement.src = `https://openweathermap.org/img/w/${icon}.png`;
+    // iconElement.src = `https://openweathermap.org/img/w/${icon}.png`;
+    iconElement.src = `/src/weather-icons/animated/cloudy-day-3.svg`;
+
+    const dayElement = document.createElement("div");
+    dayElement.classList.add("day");
 
     const tempElement = document.createElement("div");
     tempElement.classList.add("temp");
     tempElement.textContent = `${temp}°C`;
 
-    dayElement.appendChild(dayNameElement);
-    dayElement.appendChild(dateElement);
+    const dayDateElement = document.createElement("div");
+
+    const dayNameElement = document.createElement("p");
+    dayNameElement.classList.add("day-name");
+    dayNameElement.textContent = dayName;
+
+    const dateElement = document.createElement("p");
+    dateElement.classList.add("date");
+    dateElement.textContent = date.toLocaleDateString("en-US");
+
     dayElement.appendChild(iconElement);
     dayElement.appendChild(tempElement);
+
+    dayDateElement.appendChild(dayNameElement);
+    dayDateElement.appendChild(dateElement);
+
+    dayElement.appendChild(dayDateElement);
 
     forecastDiv.appendChild(dayElement);
   });
