9df7c394adab331146cfcb03b02eab296734029d
Mo Mahboobian <momahboobian@gmail.com>
2024-11-20 00:38:41 +0000

    Initial update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let message = "Bye there"
+
+  if (Math.random() > 0.5 ) {
+    message = "Hello there"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{message}</div>
     </div>
   );
 }
